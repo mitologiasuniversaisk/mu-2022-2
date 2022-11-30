a19115f2845c7fb159bac4fd5fb1e0a545387b4e
Nicole Sousa <nicolesousa@alu.ufc.br>
2022-11-30 17:31:05 -0300

    modified:   css/main.css 	new file:   css/timeline.css 	new file:   font/Augusta/Augusta-Shadow.ttf 	new file:   font/Augusta/Augusta.ttf 	renamed:    img/book/capa.jpg -> img/book/capas/capa-1.jpg 	new file:   img/book/capas/capa-2.png 	new file:   img/book/capas/capa-3.png 	new file:   img/book/capas/capa-4.png 	new file:   img/book/capas/capa-5.png 	new file:   img/book/capas/capa-6.png 	new file:   img/book/capas/capa-7.png 	new file:   img/book/capas/capa.png 	new file:   img/book/capas/contra-capa.png 	new file:   img/book/texturas/asfalt-dark.png 	new file:   img/book/texturas/light-paper-fibers.png 	new file:   img/book/texturas/vintage-grunge-paper-2.png 	new file:   img/book/texturas/vintage-grunge-paper.png 	new file:   img/herois/helvakyr-2.jpg 	new file:   img/herois/helvakyr.png 	new file:   img/herois/kyla-2.png 	new file:   img/herois/kyla.png 	new file:   img/herois/sem-nome.png 	new file:   img/herois/sylvian.png 	new file:   img/mapa/mapa-2.png 	renamed:    img/mapa.jpg -> img/mapa/mapa.jpg 	new file:   img/parte1.png 	new file:   img/parte2.png 	new file:   img/parte3.png 	new file:   img/simbolos/logo-branca.png 	new file:   img/simbolos/logo-preto.png 	modified:   index.html 	modified:   js/init.js

--- js/init.js
+++ js/init.js
@@ -13,5 +13,58 @@ $(function(){
         height: 600,
         next: '#next_book',
         prev: '#prev_book'
-    })
+    });
+    $('#pg-0').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "4");
+    });
+    $('#pg-1').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "5");
+    });
+    $('#pg-2').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "6");
+    });
+    $('#pg-3').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "8");
+    });
+    $('#pg-4').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "9");
+    });
+    $('#pg-5').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "11");
+    });
+    $('#pg-6').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "13");
+    });
+    $('#pg-7').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "15");
+    });
+    $('#pg-8').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "16");
+    });
+    $('#pg-9').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "20");
+    });
+    $('#pg-10').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "22");
+    });
+    $('#pg-11').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "24");
+    });
+    $('#pg-12').click(function(e){
+        e.preventDefault();
+        $('#margord-book').booklet("gotopage", "27");
+    });
 });
+
